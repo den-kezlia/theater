e325fef83df11143587d1abed89f1e45a0e02aaa
Den Kezlia <den.kezlia@gmail.com>
2021-03-12 19:19:41 +0200

    Implemented basic html file generator from airtable

--- server/generateEvents.js
+++ server/generateEvents.js
@@ -1,81 +1,165 @@
 const config = require('../config/config.json');
 const Airtable = require('airtable');
+const tr = require('transliteration');
 const fs = require('fs');
 const path = require('path');
+const http = require('https');
 
 Airtable.configure({
     endpointUrl: config.apiUrl,
     apiKey: config.apiKey
 });
-const IDTable = Airtable.base(config.ID);
 
-const getIDs = () => {
-    const promise = (resolve, reject) => {
-        const IDs = [];
+const generateFolderName = (name) => {
+    return tr.transliterate(name, config.transliterationOptions).toLocaleLowerCase().split(' ').join('_')
+}
 
-        IDTable('IDs').select({
-            view: config.view
-        }).eachPage(function page(records) {
-            records.forEach(function(record) {
-                IDs.push(record.get('ID'))
-            });
+async function parsePerformanceRecords(records) {
+    let performances = [];
 
-            resolve(IDs)
-        }, function done(err) {
-            if (err) { console.error(err); reject(); return; }
-        });
+    for (let record of records) {
+        if (record.get('Name')) {
+            let performance = {
+                name: record.get('Name'),
+                description: record.get('Description'),
+                image: record.get('Image'),
+                address: record.get('Address'),
+                duration: record.get('Duration'),
+                events: []
+            }
+
+            if (record.get('Events')) {
+                let events = await getEvents(record.get('Events').split(','));
+                performance.events = events;
+            }
+
+            performances.push(performance)
+        }
     }
 
+    return performances;
+}
+
+const getPerformances = () => {
+    let promise = ((resolve, reject) => {
+        let PerformancesTable = Airtable.base(config.ID);
+        PerformancesTable('List').select().firstPage(async (err, records) => {
+            if (err) { reject(err); return; }
+
+            resolve(await parsePerformanceRecords(records));
+        });
+    });
+
     return new Promise(promise);
+}
+
+const getEvents = (IDs) => {
+    return Promise.all(IDs.map(getEvent));
 }
 
 const getEvent = (id) => {
-    const promise = (resolve, reject) => {
-        const eventTable = Airtable.base(id);
+    let promise = (resolve, reject) => {
+        let eventTable = Airtable.base(id);
 
-        eventTable('Description').select({
-            view: config.view
-        }).eachPage(function page(records) {
-            records.forEach(function(record) {
-                const eventData = {};
-                eventData.date = record.get('Date');
-                eventData.address = record.get('Address');
-                eventData.description = record.get('Description');
+        eventTable('Details').select().firstPage((err, records) => {
+            if (err) { reject(err); return; }
 
-                resolve(eventData);
+            resolve({
+                date: records[0].get('Date'),
+                name: records[0].get('Name')
             });
-        }, function done(err) {
-            if (err) { console.error(err); reject(err); return; }
         });
     }
 
     return new Promise(promise);
 }
 
-const generateEventHTML = (data) => {
-    return `---
-    title: Спектакль "Гавриилиада"
-    eventTitle: Гавриилиада
-    description: ${data.description}
-    date: ${data.date}
-    address: ${data.address}
-    images:
-        main: images/main.jpg
-    ---`;
+const generateFolders = (folders) => {
+    folders.map(folder => {
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder);
+        }
+    })
 }
 
-const generateEventFile = (html) => {
-    let filename = path.resolve(__dirname, `../src/events/test/index.html`);
+const generateImages = (folder, image) => {
+    // TODO: implement webp images
+    let mainImage = {
+        path: path.resolve(folder, 'main.jpg'),
+        url: image.url
+    };
+    let thumbImage = {
+        path: path.resolve(folder, 'thumb.jpg'),
+        url: image.thumbnails.large.url
+    };
+    let images = [mainImage, thumbImage];
 
-    fs.appendFile(filename, html, function (err) {
+    images.map(img => {
+        let imgFile = fs.createWriteStream(img.path);
+
+        http.get(img.url, data => {
+            data.pipe(imgFile);
+        });
+    })
+}
+
+const generatePerformance = (performance) => {
+    let html = generatePerformanceHTML(performance);
+    let folderName = generateFolderName(performance.name);
+    let src = path.resolve(__dirname, `../${config.srcPerformancePath}`);
+    let folderPath = path.resolve(src, folderName);
+    let imagePath = path.resolve(folderPath, 'images');
+
+    generateFolders([src, folderPath, imagePath]);
+    generateHTMLFile(folderPath, html);
+    generateImages(imagePath, performance.image[0]);
+}
+
+const generateHTMLFile = (folder, html) => {
+    let filePath = path.resolve(folder, 'index.html');
+
+    fs.writeFileSync(filePath, html, { flag: 'w+' }, function (err) {
         if (err) return console.log(err);
     });
 }
 
-getIDs().then(IDs => {
-    return Promise.all(IDs.map(getEvent))
-}).then(events => {
-    console.log(events)
+const getPerformanceDates = (events) => {
+    let dates = [];
+
+    events.forEach(event => {
+        dates.push(event.date);
+    })
+
+    return dates
+}
+
+const getPerformanceEventsHTML = (events) => {
+    let dates = getPerformanceDates(events);
+
+    return dates.map(date => {
+        return `  - date: ${date}\n`
+    })
+}
+
+const generatePerformanceHTML = (data) => {
+    let html = `---
+title: ${data.name}
+name: ${data.name}
+description: ${data.description}
+address: ${data.address}
+duration: ${data.duration}
+events:
+${getPerformanceEventsHTML(data.events)}
+images:
+    main: images/main.jpg,
+    thumb: images/thumb.jpg
+---`;
+
+    return html;
+}
+
+getPerformances().then(performances => {
+    performances.map(generatePerformance)
 }).catch(err => {
     console.log(err)
-})
+});
