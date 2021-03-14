3b31eeb5c15774d12e5b193f9a1e2baa61e14e11
Den Kezlia <den.kezlia@gmail.com>
2021-03-15 01:10:48 +0200

    Added performance page

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,7 @@ let performancesToggle = document.getElementById('js-performances__toggle');
 
 performancesToggle.addEventListener('click', function() {
     let block = document.getElementById('js-performances');
+
     block.classList.toggle('performances--collapsed');
+    performancesToggle.classList.toggle('performances__toggle--hidden');
 });
