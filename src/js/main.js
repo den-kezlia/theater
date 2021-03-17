ba92cd1a7eda44936837f5e6758c185131edf69e
Den Kezlia <den.kezlia@gmail.com>
2021-03-17 08:52:15 +0200

    Updated page layout and styles

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,5 @@ let performancesToggle = document.getElementById('js-performances__toggle');
 
 performancesToggle.addEventListener('click', function() {
     let block = document.getElementById('js-performances');
-
     block.classList.toggle('performances--collapsed');
-    performancesToggle.classList.toggle('performances__toggle--hidden');
 });
