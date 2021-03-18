b3f80108cdde2bdc7981fc1633b830fa732b41a1
Den Kezlia <den.kezlia@gmail.com>
2021-03-18 12:03:30 +0200

    Updated chairs position

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,41 @@
 let performanceToggle = () => {
     let performancesToggle = document.getElementById('js-performances__toggle');
 
-    performancesToggle.addEventListener('click', function() {
-        let block = document.getElementById('js-performances');
-        block.classList.toggle('performances--collapsed');
-    });
+    if (performancesToggle) {
+        performancesToggle.addEventListener('click', function() {
+            let block = document.getElementById('js-performances');
+            block.classList.toggle('performances--collapsed');
+        });
+    }
+}
+
+let _getChairSpace = () => {
+    let windowSize = document.getElementById('js-chairs').offsetWidth;
+    let chairSpace;
+
+    switch (true) {
+        case windowSize < 700:
+            chairSpace = 10;
+            break;
+        case windowSize >= 700 && windowSize < 1024:
+            chairSpace = 15;
+            break;
+        default:
+            chairSpace = 20;
+            break;
+    }
+
+    return chairSpace;
 }
 
 let sceneSize = () => {
     let calculateSceneSize = () => {
-        let windowSize = window.window.innerWidth;
-        let ellipseSize = windowSize / 2;
+        let windowSize = document.getElementById('js-chairs').offsetWidth;
+        let ellipseSize = (windowSize / 2) + 'px';
+        let chairSize = ((windowSize / 18) - _getChairSpace()) + 'px';
+
+        document.documentElement.style.setProperty('--ellipse-size', ellipseSize);
+        document.documentElement.style.setProperty('--chair-size', chairSize);
     };
 
     calculateSceneSize();
