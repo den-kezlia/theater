20e65dd53576990a7eafe57a693f97d02508900f
Den Kezlia <den.kezlia@gmail.com>
2021-03-18 09:29:50 +0200

    Updated tickets layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,27 @@
-let performancesToggle = document.getElementById('js-performances__toggle');
+let performanceToggle = () => {
+    let performancesToggle = document.getElementById('js-performances__toggle');
 
-performancesToggle.addEventListener('click', function() {
-    let block = document.getElementById('js-performances');
-    block.classList.toggle('performances--collapsed');
-});
+    performancesToggle.addEventListener('click', function() {
+        let block = document.getElementById('js-performances');
+        block.classList.toggle('performances--collapsed');
+    });
+}
+
+let sceneSize = () => {
+    let calculateSceneSize = () => {
+        let windowSize = window.window.innerWidth;
+        let ellipseSize = windowSize / 2;
+    };
+
+    calculateSceneSize();
+    window.addEventListener('resize', calculateSceneSize);
+}
+
+let App = {
+    init: () => {
+        performanceToggle();
+        sceneSize();
+    }
+};
+
+App.init();
